033a60803aece0f4bc2c5fa707aea4913f04b5cb
RaduStefanLungu <radustefanlungu@hotmail.com>
2024-03-09 17:53:37 +0100

    > installed react-icons > updated main colors CSS

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto '>
 
         <h1 className='text-3xl'>
-            Welcome to <span className='font-bold underline'>Blockta</span>
+            Welcome to <span className='font-bold underline bg-[var(--color-primary)]'>Blockta</span>
         </h1>
 
     </div>
