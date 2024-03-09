8fad6f128b968682e5fd3679b27e0c72eb34f465
Blxtch <moreau.romain@hotmail.com>
2024-03-09 23:28:08 +0100

    merge

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto '>
 
-        <h1 className='text-3xl'>
-            Welcome to <span className='font-bold underline'>Blockta</span>
-        </h1>
+        <HeroTitle TitleName={"Blockta2"} />
 
     </div>
   )
