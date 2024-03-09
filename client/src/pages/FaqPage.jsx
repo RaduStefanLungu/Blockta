cfe7ded6420998917f3778bfe7207c3fbf29fc8f
Maxime Delhaye <maxime.delhaye28@gmail.com>
2024-03-09 20:03:27 +0100

    bug mais je vais manger

--- client/src/pages/FaqPage.jsx
+++ client/src/pages/FaqPage.jsx
@@ -1,28 +1,67 @@
-import React, { useState } from 'react'
-import "../styles/faqPage.css"
+import React, { useState } from 'react';
+import "../styles/faqPage.css";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
-
 
 export default function FaqPage() {
   const [showResults, setShowResults] = useState(false);
 
+  const toggleResults = () => {
+    setShowResults(!showResults);
+  };
+
+  const Questions = (question) => {
+    return (
+      <h2 className="text-2xl font-bold">{question}</h2>
+    );
+  };
+
+  const Results = ({ comment }) => {
+    return (
+      <p className="text-lg">{comment}</p>
+    );
+  };
+
+  
   return (
     <>
       <div className="container mx-auto px-4">
         <h1 className="text-4xl font-bold text-center my-8">Frequently Asked Questions</h1>
-        <div className={`flex flex-col my-4 questions`} >
-          <h2 className="text-2xl font-bold">What is the purpose of this website?</h2>
-          { showResults ? <Results /> : null }
-          { showResults ? <FaChevronUp className='mx-auto my-auto text-xl icon' onClick={() => setShowResults(!showResults)}/> : <FaChevronDown className='mx-auto my-auto text-xl icon' onClick={() => setShowResults(!showResults)}/> }
+        <div className={`flex flex-col my-1 questions`} >
+          { Questions("What is this website about?") }
+          { showResults ? <Results comment="This website aims to..." /> : null }
+          { showResults ? <FaChevronUp className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> : <FaChevronDown className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> }
+        </div>
+
+        <div className={`flex flex-col my-1 questions`} >
+          { Questions("What is this website about?") }
+          { showResults ? <Results comment="This website aims to..." /> : null }
+          { showResults ? <FaChevronUp className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> : <FaChevronDown className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> }
+        </div>
+
+        <div className={`flex flex-col my-1 questions`} >
+          { Questions("What is this website about?") }
+          { showResults ? <Results comment="This website aims to..." /> : null }
+          { showResults ? <FaChevronUp className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> : <FaChevronDown className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> }
+        </div>
+
+        <div className={`flex flex-col my-1 questions`} >
+          { Questions("What is this website about?") }
+          { showResults ? <Results comment="This website aims to..." /> : null }
+          { showResults ? <FaChevronUp className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> : <FaChevronDown className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> }
+        </div>
+
+        <div className={`flex flex-col my-1 questions`} >
+          { Questions("What is this website about?") }
+          { showResults ? <Results comment="This website aims to..." /> : null }
+          { showResults ? <FaChevronUp className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> : <FaChevronDown className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> }
+        </div>
+
+        <div className={`flex flex-col my-1 questions`} >
+          { Questions("What is this website about?") }
+          { showResults ? <Results comment="This website aims to..." /> : null }
+          { showResults ? <FaChevronUp className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> : <FaChevronDown className='mx-auto my-auto text-xl icon' onClick={toggleResults}/> }
         </div>
       </div>
     </>
-  )
+  );
 }
-
-function Results() {
-  return (
-    <p className="text-lg">The purpose of this website is to provide a platform for people to share their experiences and learn from others. We believe that everyone has a story to tell and that everyone can learn from others.</p>
-  )
-}
-
