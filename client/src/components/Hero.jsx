1c48df176fd598880c40f5434ef810fba7c41004
RaduStefanLungu <radustefanlungu@hotmail.com>
2024-03-09 18:07:24 +0100

    examples Hero + Update colors index

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,10 +4,17 @@ export default function Hero() {
   return (
     <div className='container mx-auto '>
 
-        <h1 className='text-3xl'>
-            Welcome to <span className='font-bold underline bg-[var(--color-primary)]'>Blockta</span>
-        </h1>
+        <HeroTitle TitleName={"Blockta"} />
 
     </div>
   )
+}
+
+
+const HeroTitle = ({TitleName}) => {
+    return(
+        <h1 className='text-3xl'>
+            Welcome to <span className='font-bold underline bg-[var(--color-primary)]'> {TitleName} </span>
+        </h1>
+    )
 }
