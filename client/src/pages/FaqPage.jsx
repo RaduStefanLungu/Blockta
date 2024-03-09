04b72d0c0f19219b926b19c80e4cd9f9d5cdb536
Maxime Delhaye <maxime.delhaye28@gmail.com>
2024-03-09 19:41:17 +0100

    Feature template de question abouti

--- client/src/pages/FaqPage.jsx
+++ client/src/pages/FaqPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import "../styles/faqPage.css"
+import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
+
 
 export default function FaqPage() {
   const [showResults, setShowResults] = useState(false);
@@ -8,11 +10,10 @@ export default function FaqPage() {
     <>
       <div className="container mx-auto px-4">
         <h1 className="text-4xl font-bold text-center my-8">Frequently Asked Questions</h1>
-        <div className="flex flex-col">
-          <div className="flex flex-col my-4 questions" onClick={() => setShowResults(!showResults)}>
-            <h2 className="text-2xl font-bold">What is the purpose of this website?</h2>
-            { showResults ? <Results /> : null }
-          </div>
+        <div className={`flex flex-col my-4 questions`} >
+          <h2 className="text-2xl font-bold">What is the purpose of this website?</h2>
+          { showResults ? <Results /> : null }
+          { showResults ? <FaChevronUp className='mx-auto my-auto text-xl icon' onClick={() => setShowResults(!showResults)}/> : <FaChevronDown className='mx-auto my-auto text-xl icon' onClick={() => setShowResults(!showResults)}/> }
         </div>
       </div>
     </>
@@ -24,3 +25,4 @@ function Results() {
     <p className="text-lg">The purpose of this website is to provide a platform for people to share their experiences and learn from others. We believe that everyone has a story to tell and that everyone can learn from others.</p>
   )
 }
+
