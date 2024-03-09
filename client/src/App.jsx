1b518b4cb63745773dc08a9ae7e7291d927dda28
RaduStefanLungu <radustefanlungu@hotmail.com>
2024-03-09 18:19:59 +0100

    updated routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
 
 import { Routes, Route, BrowserRouter} from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import RegisterPage from './pages/RegisterPage'
+import FaqPage from './pages/FaqPage'
+import PortfolioPage from './pages/PortfolioPage'
+import ServicesPage from './pages/ServicesPage'
+import ContactPage from './pages/ContactPage'
+
 
 function App() {
 
@@ -10,11 +16,11 @@ function App() {
       <Routes>
 
         <Route path='/' element={<HomePage/>} />
-        <Route path='/register' element={<HomePage/>} />
-        <Route path='/faq' element={<HomePage/>} />
-        <Route path='/portfolio' element={<HomePage/>} />
-        <Route path='/services' element={<HomePage/>} />
-        <Route path='/contact' element={<HomePage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+        <Route path='/faq' element={<FaqPage/>} />
+        <Route path='/portfolio' element={<PortfolioPage/>} />
+        <Route path='/services' element={<ServicesPage/>} />
+        <Route path='/contact' element={<ContactPage/>} />
 
       </Routes>
 
