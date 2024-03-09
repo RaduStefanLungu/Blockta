387bbce1f834f46f92aa986f1b98799e4a2010d8
RaduStefanLungu <radustefanlungu@hotmail.com>
2024-03-09 19:50:36 +0100

    font update

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -13,8 +13,8 @@ export default function Hero() {
 
 const HeroTitle = ({TitleName}) => {
     return(
-        <h1 className='text-3xl'>
-            Welcome to <span className='font-bold underline bg-[var(--color-primary)]'> {TitleName} </span>
+        <h1 className='text-3xl font-montserrat'>
+            Welcome to <span className='font-bold underline bg-[var(--color-primary)] text-[var(--color-secondary)] px-3 py-1'> {TitleName} </span>
         </h1>
     )
 }
