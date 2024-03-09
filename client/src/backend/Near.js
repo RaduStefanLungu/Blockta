5b34d8db41f8a58ca19c44cd97f9a2dde8b5ec7c
RaduStefanLungu <radustefanlungu@hotmail.com>
2024-03-09 17:06:25 +0100

    first-depo[

---
+++ client/src/backend/Near.js
@@ -0,0 +1,19 @@
+// creates keyStore from a provided file
+// you will need to pass the location of the .json key pair
+
+const { KeyPair, keyStores } = require("near-api-js");
+const fs = require("fs");
+const homedir = require("os").homedir();
+
+const ACCOUNT_ID = "near-example.testnet"; // NEAR account tied to the keyPair
+const NETWORK_ID = "testnet";
+// path to your custom keyPair location (ex. function access key for example account)
+const KEY_PATH = "/.near-credentials/near-example-testnet/get_token_price.json";
+
+const credentials = JSON.parse(fs.readFileSync(homedir + KEY_PATH));
+const myKeyStore = new keyStores.InMemoryKeyStore();
+myKeyStore.setKey(
+  NETWORK_ID,
+  ACCOUNT_ID,
+  KeyPair.fromString(credentials.private_key)
+);
